485ed0102ebe423b4b0b5cd8bef998790d12123f
Carnagiul <pierrequeruel@gmail.com>
2025-06-13 17:05:52 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'Duck Jumelles'
+
+version '1.0.0'
+
+server_scripts {
+	'server/main.lua',
+}
+
+client_scripts {
+	'client/main.lua',
+}
+
+
+dependencies {
+	'es_extended',
+}
