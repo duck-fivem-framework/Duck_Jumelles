77ca34522dd3b46b183896b8c5e4d7b384f8cd89
Carnagiul <pierrequeruel@gmail.com>
2025-06-13 18:50:11 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,17 +1,11 @@
 local fov_max = 150.0
 local fov_min = 7.0    -- max zoom (plus petit = plus fort zoom)
-local zoomspeed = 10.0 -- vitesse de zoom
 local speed_lr = 8.0   -- rotation gauche/droite
 local speed_ud = 8.0   -- rotation haut/bas
 local zoomstep = 5.0
 
-local toggle_helicam    = 51  -- INPUT_CONTEXT (E)
-local toggle_rappel    = 154  -- INPUT_DUCK (X)
-local toggle_spotlight = 183  -- INPUT_PHONE_CAMERA_GRID (G)
-local toggle_lock_on   = 22   -- INPUT_SPRINT (SPACE)
 
 local helicam = false
-local polmav_hash = GetHashKey("pcj")
 local fov = (fov_max + fov_min) * 0.5
 local vision_state = 0  -- 0=normal, 1=nightmode, 2=thermal
 
@@ -22,7 +16,7 @@ local _cam         = nil
 local _heliEntity  = nil
 
 local function exitJumelles()
-     if helicam and _cam then
+    if helicam and _cam then
     local ped = PlayerPedId()
 
     -- joue le son de fermeture
@@ -35,20 +29,27 @@ local function exitJumelles()
 end
 
 -- Incrémente le zoom
-RegisterCommand('+duckJumelleZoomIncrement', function()
+
+local function zoomIncrement()
+    print("duckJumelleZoomIncrement")
+        
     if helicam and _cam then
         fov = math.max(fov_min, fov - zoomstep)
         SetCamFov(_cam, fov)
     end
-end)
+end
 
--- Décrémente le zoom
-RegisterCommand('+duckJumelleZoomDecrement', function()
+local function zoomDecrement()
+    print("duckJumelleZoomDecrement")
     if helicam and _cam then
         fov = math.min(fov_max, fov + zoomstep)
         SetCamFov(_cam, fov)
     end
-end)
+end
+
+-- Décrémente le zoom
+RegisterCommand('+duckJumelleZoomDecrement', zoomDecrement)
+RegisterCommand('+duckJumelleZoomIncrement', zoomIncrement)
 RegisterCommand('+duckJumellesExit', exitJumelles)
 -- KeyMappings pour binder les touches A/E
 RegisterKeyMapping('+duckJumelleZoomIncrement', 'Jumelles Zoom +', 'keyboard', 'Q')
@@ -190,87 +191,3 @@ AddEventHandler('duck:jumelles:active', function(vision)
     -- vision doit être un string "normal", "night" ou "thermal"
     setVision(vision)
 end)
-
---FUNCTIONS--
-
-function IsPlayerInPolmav()
-	local lPed = GetPlayerPed(-1)
-	local vehicle = GetVehiclePedIsIn(lPed)
-	return IsVehicleModel(vehicle, polmav_hash)
-end
-
-
-function HideHUDThisFrame()
-	HideHelpTextThisFrame()
-	HideHudComponentThisFrame(19) -- weapon wheel
-	HideHudComponentThisFrame(1) -- Wanted Stars
-	HideHudComponentThisFrame(2) -- Weapon icon
-	HideHudComponentThisFrame(3) -- Cash
-	HideHudComponentThisFrame(4) -- MP CASH
-	HideHudComponentThisFrame(13) -- Cash Change
-	HideHudComponentThisFrame(11) -- Floating Help Text
-	HideHudComponentThisFrame(12) -- more floating help text
-	HideHudComponentThisFrame(15) -- Subtitle Text
-	HideHudComponentThisFrame(18) -- Game Stream
-end
-
-function CheckInputRotation(cam, zoomvalue)
-	local rightAxisX = GetDisabledControlNormal(0, 220)
-	local rightAxisY = GetDisabledControlNormal(0, 221)
-	local rotation = GetCamRot(cam, 2)
-	if rightAxisX ~= 0.0 or rightAxisY ~= 0.0 then
-		new_z = rotation.z + rightAxisX*-1.0*(speed_ud)*(zoomvalue+0.1)
-		new_x = math.max(math.min(20.0, rotation.x + rightAxisY*-1.0*(speed_lr)*(zoomvalue+0.1)), -89.5) -- Clamping at top (cant see top of heli) and at bottom (doesn't glitch out in -90deg)
-		SetCamRot(cam, new_x, 0.0, new_z, 2)
-	end
-end
-
-function HandleZoom(cam)
-	local lPed = GetPlayerPed(-1)
-	if not ( IsPedSittingInAnyVehicle( lPed ) ) then
-
-		if IsControlJustPressed(0,32) then -- Scrollup
-			fov = math.max(fov - zoomspeed, fov_min)
-		end
-		if IsControlJustPressed(0,8) then
-			fov = math.min(fov + zoomspeed, fov_max) -- ScrollDown		
-		end
-		local current_fov = GetCamFov(cam)
-		if math.abs(fov-current_fov) < 0.1 then -- the difference is too small, just set the value directly to avoid unneeded updates to FOV of order 10^-5
-			fov = current_fov
-		end
-		SetCamFov(cam, current_fov + (fov - current_fov)*0.05) -- Smoothing of camera zoom
-	else
-		if IsControlJustPressed(0,241) then -- Scrollup
-			fov = math.max(fov - zoomspeed, fov_min)
-		end
-		if IsControlJustPressed(0,242) then
-			fov = math.min(fov + zoomspeed, fov_max) -- ScrollDown		
-		end
-		local current_fov = GetCamFov(cam)
-		if math.abs(fov-current_fov) < 0.1 then -- the difference is too small, just set the value directly to avoid unneeded updates to FOV of order 10^-5
-			fov = current_fov
-		end
-		SetCamFov(cam, current_fov + (fov - current_fov)*0.05) -- Smoothing of camera zoom
-	end
-end
-
-function GetVehicleInView(cam)
-	local coords = GetCamCoord(cam)
-	local forward_vector = RotAnglesToVec(GetCamRot(cam, 2))
-	--DrawLine(coords, coords+(forward_vector*100.0), 255,0,0,255) -- debug line to show LOS of cam
-	local rayhandle = CastRayPointToPoint(coords, coords+(forward_vector*200.0), 10, GetVehiclePedIsIn(GetPlayerPed(-1)), 0)
-	local _, _, _, _, entityHit = GetRaycastResult(rayhandle)
-	if entityHit>0 and IsEntityAVehicle(entityHit) then
-		return entityHit
-	else
-		return nil
-	end
-end
-
-function RotAnglesToVec(rot) -- input vector3
-	local z = math.rad(rot.z)
-	local x = math.rad(rot.x)
-	local num = math.abs(math.cos(x))
-	return vector3(-math.sin(z)*num, math.cos(z)*num, math.sin(x))
-end
